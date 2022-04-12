bbef732024cbc323039a0d4ca54ed2baaae4c3cd
Robson Barros <robsonbarros@rtecdevweb.com>
2022-04-12 15:31:16 -0300

    adicionado novos arquivos inclusive readme

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>NOVO PROJETO</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="container_principal">
+	
+	<div class="header-bg">
+		<ul>
+			<li><h1>CONTROLE DE ESTOQUE<h1></li>
+		</ul>
+		<ul>
+			<li><a id="mostra-form" href="#" title="adicionar novo produto"> <i class="fa fa-plus"></i> novo produto</a></li>
+			<li><a href="#" title="listar produtos"><i class="fa fa-newspaper-o" aria-hidden="true"></i> listar produtos</a></li>
+			<li><a href="#"><i class="fa fa-barcode" aria-hidden="true"> </i> ler codigo de barra</li></a></li>
+			
+			<li>listar de produtos</li>
+			<li>imprimir nota fiscal</li>
+			<li>criar boleto bancário</li>
+
+		</ul>
+
+</div>
+
+</div><!--container principal-->
+</body>
+</html>
